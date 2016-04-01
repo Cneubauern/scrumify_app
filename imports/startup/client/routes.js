d67eda2ee8c0ef284680ba66d882720997cd8f42
cneubauern <cneubauern@googlemail.com>
2016-04-02 01:28:28 +0200

    Added routes for static pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,8 +2,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 //import { AccountsTemplates } from 'meteor/useraccounts:core';
 
-import '/imports/ui/layouts/App_body.js'
-import '/imports/ui/components/Chat.js'
+import '/imports/ui/layouts/App_body.js';
+import '/imports/ui/components/Chat.js';
+import '/imports/ui/pages/App_notFound.js';
+import '/imports/ui/pages/About_page.js';
+import '/imports/ui/pages/Agb_page.js';
+import '/imports/ui/pages/Faq_page.js';
+import '/imports/ui/pages/Impressum_page.js';
 
 FlowRouter.route('/', {
     name: 'App.home',
@@ -17,3 +22,29 @@ FlowRouter.notFound = {
         BlazeLayout.render('App_body', {main: 'App_notFound'});
     }
 };
+
+
+FlowRouter.route('/about', {
+    name: 'App.about',
+    action() {
+        BlazeLayout.render('App_body', {main: 'About_page'});
+    }
+});
+FlowRouter.route('/agb', {
+    name: 'App.agb',
+    action() {
+        BlazeLayout.render('App_body', {main: 'Agb_page'});
+    }
+});
+FlowRouter.route('/faq', {
+    name: 'App.faq',
+    action() {
+        BlazeLayout.render('App_body', {main: 'Faq_page'});
+    }
+});
+FlowRouter.route('/impressum', {
+    name: 'App.impressum',
+    action() {
+        BlazeLayout.render('App_body', {main: 'Impressum_page'});
+    }
+});
